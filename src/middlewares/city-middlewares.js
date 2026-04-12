cbaddda774dc8952423b95adec1d961295329584
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2026-04-12 16:32:51 +0530

    add city functionality and added schema for the airport

---
+++ src/middlewares/city-middlewares.js
@@ -0,0 +1,15 @@
+const { StatusCodes } = require('http-status-codes');
+const { ErrorResponse } = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+function validateCreateRequest(req, res, next) {
+    if (!req.body.name) {
+        ErrorResponse.message = 'Something went wrong while creating city';
+        ErrorResponse.error = new AppError(['City Name Number not found'], StatusCodes.BAD_REQUEST);
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+    next();
+}
+
+module.exports = {
+    validateCreateRequest
+};
