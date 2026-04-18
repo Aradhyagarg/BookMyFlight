23a3125a824746d74fdb670366350a40e77e1f73
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2026-04-18 16:16:18 +0530

    added airplane airport city

--- src/index.js
+++ src/index.js
@@ -6,7 +6,34 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server at PORT : ${ServerConfig.PORT}`);
     Logger.info("Successfully run server", {});
+    const { City, Airport } = require('./models');
+
+  const bengaluru = await City.findByPk(1);
+  console.log(bengaluru);
+
+  /*if (!bengaluru) {
+    console.log("City not found");
+    return;
+  }*/
+
+  //const bengaluru = await City.create({
+    name: 'Bengaluru'
+  //});
+
+  // create airport via association (best way)
+  /*const airport = await bengaluru.createAirport({
+    name: 'Kempegowda Airport',
+    code: 'BLR',
+    address: 'Bangalore'
+  });*/
+
+  //console.log("Created Airport:", airport);
+
+  // fetch all airports of this city
+  const airportsInBlr = await bengaluru.getAirports();
+  console.log("Airports in BLR:", airportsInBlr);
+
 })
--- src/models/airport.js
+++ src/models/airport.js
@@ -11,6 +11,17 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.City, {
+        foreignKey: 'cityId', onDelete: 'CASCADE'
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: 'departureAirportId',
+        onDelete: 'CASCADE'
+      });
+      this.hasMany(models.Flight, {
+        foreignKey: 'arrivalAirportId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Airport.init({
@@ -30,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
       unique: true
     },
     cityId: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
       unique: true
     }
